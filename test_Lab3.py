0daffe80b82d03c013c2f907473c6dd7d7398c34
Kendrick <khookendrick@gmail.com>
2025-11-30 11:04:22 +0800

    EX 3

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,23 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_10ormore():
+    #Act
+    input_arr = [10,10,10,1,1,1,2,3,4,5,6,7]
+
+    #arrange
+    result = Lab3.bubble_sort(input_arr, test_bubble_sort_descending)
+
+    #assert
+    assert result == 1
+
+def test_non_integervalues():
+    #act
+    input_array = [1,2,3,4,5,6,8,"hello",5]
+
+    #arrange
+    result = Lab3.bubble_sort(input_array, test_bubble_sort_descending)
+
+    #assert
+    assert result == 2
